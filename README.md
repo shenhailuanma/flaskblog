0d6783d95d52dee728a9f8d68418e93ef4886e03
zhangxu <zx051212@gmail.com>
2014-01-07 10:06:07 +0800

    at the begin(v1.0)
    at the begin(v1.0), add the files.

---
+++ README.md
@@ -0,0 +1,32 @@
+weblog
+======
+
+个人博客。
+
+[Example: 马孔多的主页](http://makongduo.tk)
+
+Download
+--------
+    $ git clone https://github.com/ghostrong/weblog.git
+
+Quick Start
+-----------
+
+- 配置数据库连接参数：config.py
+- 创建table
+
+    $ python data.py
+- 运行 app
+
+    $ python myapp.py
+
+通过Admin 管理数据
+------------------
+
+    http://localhost:5555/blog/admin/
+
+注意根据需要修改访问权限。
+
+
+
+
---
+++ myapp.py
@@ -0,0 +1,22 @@
+#coding=utf8
+
+import sys
+import os
+
+CSRF_ENABLED = False
+#SECRET_KEY = 'you-will-never-guess'
+
+
+abspath = os.path.abspath(__file__)
+app_root = os.path.dirname(abspath)
+path = os.path.join(app_root, 'virtualenv.bundle')
+sys.path.insert(0, path)
+sys.path.insert(0, app_root)
+
+from flask import Flask
+app = Flask(__name__)
+app.config.from_object('config')
+from views import *
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10386, debug=True)
